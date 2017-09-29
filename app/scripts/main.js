639cfe4ef4569ee9f9d432737804bfe3c8ccda9e
ValeriiSergheiev <Val.Serheiev@gmail.com>
2017-09-29 22:57:42 +0300

    Customers slider added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 
 // wow init
-new WOW().init();
+var wow = new WOW(
+{
+	mobile: false
+}
+)
+wow.init();
 
 //hamburgers
 $('.hamburger').click(function() {
@@ -30,6 +35,27 @@ $('.main-slider').slick({
 	]
 });
 
+$('.slider-customers').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: true,
+	arrows: false,
+	//fade: true,
+	speed: 700,
+	autoplay: true,
+	pauseOnHover: false,
+	autoplaySpeed: 2000,
+	responsive: [
+	{
+		breakpoint: 992,
+		settings: {
+			dots: false
+		}
+	}
+	]
+});
+
 //sidebar menu 
 /*$('.sidebar-btn button').click(function() {
 	//$('#sidenav').width(370);
@@ -41,5 +67,10 @@ $('.main-slider').slick({
 //Map height
 var mapHeight = $('#map').width();
 $('#map').height(mapHeight);
+
+$(window).resize(function() {
+	var mapHeight = $('#map').width();
+	$('#map').height(mapHeight);	
+});
 
 });
